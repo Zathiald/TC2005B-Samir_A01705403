8d77143a593829e37487f17f1bc81c060908906d
Zathiald <A01705403.com>
2024-02-15 22:04:53 -0600

    Agregar funciones finales para lab4

--- Lab1_Archives/JavaScript_Testing.js
+++ Lab1_Archives/JavaScript_Testing.js
@@ -6,9 +6,9 @@ console.assert(1==1);
 
 // 1. Tabla de cuadrados y cubos
 let num = prompt("Por favor, introduce un numero");
-document.write("<table><tr><th><i>Numero</i></th><th>Cuadrado</th><th>Cubo</th></tr>");
+document.write("<table><tr><th><i>Numero</i></th><th><i>Cuadrado</i></th><th><i>Cubo</i></th></tr>");
 for(let i = 1; i <= num; i++){
-    document.write("<tr><td>" + i + "</td><td>" + i*i + "</td><td>" + i*i*i + "</td></tr>");
+    document.write("<tr><td><i>" + i + "</i></td><td>" + "<i>" + i*i + "</td><td></i>" + "<i>" + i*i*i + "</i></td></tr>");
 }
 document.write("</table>");
 
@@ -30,7 +30,7 @@ function contador(arr){
     let negativos = arr.filter(x => x < 0).length;
     let ceros = arr.filter(x => x === 0).length;
     let positivos = arr.filter(x => x > 0).length;
-    return [negativos, ceros, positivos];
+    return "Numero de negativos: " + negativos + "\nNumero de ceros:" + ceros + "\nNumero de positivos:" + positivos;
 }
 
 var arreglo=[-5,-4,-3,-2,-1,0,1,2,3,4,5];
@@ -44,7 +44,8 @@ console.log(result);
 
 // 4. Promedios
 function promedios(arr){
-    return arr.map(x => x.reduce((a, b) => a + b, 0) / x.length);
+    var result = arr.map(x => x.reduce((a, b) => a + b, 0) / x.length)
+    return "El promedio es: " + result + "\n";
 }
 
 
@@ -58,7 +59,8 @@ console.log(result2);
 
 // 5. Inverso
 function inverso(num){
-    return parseInt(num.toString().split('').reverse().join(''));
+    var reverse = parseInt(num.toString().split('').reverse().join(''))
+    return "El numero al reves es: " + reverse;
 }
 
 var numero=12345;
@@ -68,6 +70,49 @@ var result3=inverso(numero);
 console.log("Pregunta 5")
 console.log(numero);
 console.log(result3);
+
+//6.Calcular Factorial
+class FactorialCalculator {
+    constructor(number) {
+        this.number = number;
+    }
+
+    validateNumber() {
+        if (this.number < 0) {
+            return false;
+        }
+        return true;
+    }
+
+    calculateFactorial() {
+        let result = 1;
+        for (let i = 2; i <= this.number; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
+
+function calcularFactorial() {
+    let number = document.getElementById('number').value;
+    let calculator = new FactorialCalculator(number);
+    if (calculator.validateNumber()) {
+        let result = calculator.calculateFactorial();
+        document.getElementById('result').innerHTML = "<p class='r'><em> El factorial de " + number  + " es " + result + "</p></em>" ;
+    } else {
+        document.getElementById('result').innerHTML = "<p class='r'><em>Por favor, ingresa un numero entero no negativo.</p></em>";
+    }
+}
+
+
+document.write("<h3><em> Problema 6</em></h3>");
+document.write("<p class='r'><em>El calculo del factorial de un numero es el producto de todos los numeros enteros positivos desde 1 hasta ese numero, por ejemplo el factorial de 5 es 1*2*3*4*5=120</p></em>");
+document.write("<input type='number' id='number' placeholder='Ingresa un numero'>");
+document.write("<button onclick='calcularFactorial()'> Calcular Factorial</button>");
+document.write("<p id='result'></p>");
+
+
+
 
 
 
